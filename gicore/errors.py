3d02cb2f44cf7b49981786b75bbc5c20bfa00c80
brownhead <jsull003@ucr.edu>
2013-09-18 16:37:09 -0700

    Added CriticalError exception.
    * When the fecal matter hits the rotating air disruptor its time to
      throw up your arms and raise some heavy-handed errors.

--- gicore/errors.py
+++ gicore/errors.py
@@ -15,5 +15,26 @@ class VerificationError(Exception):
 
 		Exception.__init__(self, *args, **kwargs)
 
+	def __repr__(self):
+		return "VerificationError(url = %s)" % (self.url, )
+
 	def __str__(self):
 		return "File at '%s' failed verification." % (self.url, )
+
+class CriticalError(Exception):
+	"""
+	Exception raised when critical assumptions are not met. Human intervention
+	nearly always required.
+
+	"""
+
+	def __init__(self, message, *args, **kwargs):
+		self.message = message
+		Exception.__init__(self, *args, **kwargs)
+
+	def __repr__(self):
+		return "CriticalError(%s)" % (message, )
+
+	def __str__(self):
+		msg = "A critical error has occurred: %s" % (message, )
+		return msg
